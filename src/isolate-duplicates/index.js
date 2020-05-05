5536fc70dbd6866d633d47dfbed331a0a7ecc88a
David Oparanti <david.oparanti@gmail.com>
2020-05-05 23:02:37 +0100

    Solved isolate-duplicate

--- src/isolate-duplicates/index.js
+++ src/isolate-duplicates/index.js
@@ -1,3 +1,26 @@
-function isolateDuplicates(text) {}
+function isolateDuplicates(text) {
+    let countDuplicates = 0
+    let output = "";
+    let stringCount = {};
+
+    for (let char of text) {
+        if (stringCount[char]) {
+            stringCount[char] += 1
+        } else {
+            stringCount[char] = 1;
+        }
+    }
+
+    for (let char in stringCount) {
+        if (stringCount[char] <= 2) {
+            output += char.repeat(stringCount[char]);
+        } else {
+            countDuplicates++;
+            output += char + char + "[" + char.repeat(stringCount[char] - 2) + "]";
+        }
+    }
+
+    return "[" + output + "," + countDuplicates + "]";
+}
 
 module.exports = isolateDuplicates;
